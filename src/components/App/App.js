8e06da45191085fa56dce563dc5cfd8b7621c0fe
nembence <nemeth.bence@icloud.com>
2020-09-01 10:33:53 +0200

    Change App components

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
