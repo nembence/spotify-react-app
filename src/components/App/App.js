54482d2fdad51aab542e071f180a4fc78d6ae4ea
magfabian <mag.fabian87@gmail.com>
2020-09-01 11:30:59 +0200

    Add routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,39 @@
 import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-import Main from "../Main/Main";
 import Home from "../Home/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
+import Favorite from "../Favorite/Favorite";
+import Category from "../Category/Category";
 
 function App() {
     return (
         <Router>
             <Header />
             <Navbar />
+
             <Switch>
                 <Route exact path="/">
                     <Home />
                 </Route>
+                <Route path="/favorite">
+                    <Favorite />
+                </Route>
+                <Route path="/artist">
+                    <Category name={"artist"} />
+                </Route>
+                <Route path="/album">
+                    <Category name={"album"} />
+                </Route>
+                <Route path="/track">
+                    <Category name={"track"} />
+                </Route>
+                <Route path="/playlist">
+                    <Category name={"playlist"} />
+                </Route>
             </Switch>
-            <Main />
+
             <Footer />
         </Router>
     );
