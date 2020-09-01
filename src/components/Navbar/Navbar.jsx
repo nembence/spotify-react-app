487a9410d776f6c3f5bcdf55cbe883397d41bf8f
nembence <nemeth.bence@icloud.com>
2020-09-01 14:34:00 +0200

    Change navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,21 +7,42 @@ const Navbar = () => {
             <NavLink activeClassName="active" exact to="/" className="item">
                 New releases
             </NavLink>
-            <NavLink activeClassName="active" to="/artist" className="item">
-                Artist
-            </NavLink>
-            <NavLink activeClassName="active" to="/track" className="item">
-                Track
-            </NavLink>
-            <NavLink activeClassName="active" to="/album" className="item">
-                Album
-            </NavLink>
-            <NavLink activeClassName="active" to="/playlist" className="item">
-                Playlist
-            </NavLink>
             <NavLink activeClassName="active" to="/favorite" className="item">
                 Favorites
             </NavLink>
+            <div class="item">
+                <p>Search by:</p>
+                <div class="menu">
+                    <NavLink
+                        activeClassName="active"
+                        to="/artist"
+                        className="item"
+                    >
+                        Artist
+                    </NavLink>
+                    <NavLink
+                        activeClassName="active"
+                        to="/track"
+                        className="item"
+                    >
+                        Track
+                    </NavLink>
+                    <NavLink
+                        activeClassName="active"
+                        to="/album"
+                        className="item"
+                    >
+                        Album
+                    </NavLink>
+                    <NavLink
+                        activeClassName="active"
+                        to="/playlist"
+                        className="item"
+                    >
+                        Playlist
+                    </NavLink>
+                </div>
+            </div>
         </nav>
     );
 };
