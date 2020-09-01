f7348214df8c189d09522e37d0d184bcb45aba1a
magfabian <mag.fabian87@gmail.com>
2020-09-01 15:25:28 +0200

    Add Cardcontainer component

---
+++ src/components/CardContainer/CardContainer.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const List = styled.ul`
+    padding: 32px;
+    text-align: center;
+    display: grid;
+    gap: 16px;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
+`;
+
+export default List;
--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SearchBar from "../SearchBar/SearchBar";
-import Loading from "../Loading/Loading";
+// import Loading from "../Loading/Loading";
+import CardContainer from "../CardContainer/CardContainer";
 
 const Category = ({ name }) => {
     return (
         <div className="content">
             <SearchBar name={name} />
-            <Loading />
+            {/* <Loading /> */}
+            <CardContainer></CardContainer>
         </div>
     );
 };
