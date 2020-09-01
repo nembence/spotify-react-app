bf0ca26d826e67a220e160a2c95ac4eb97fd71ef
nembence <nemeth.bence@icloud.com>
2020-09-01 15:11:16 +0200

    Add  SearchBar to Category

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import SearchBar from "../SearchBar/SearchBar";
+import Loading from "../Loading/Loading";
 
 const Category = ({ name }) => {
-    return <div className="content">{name}</div>;
+    return (
+        <div className="content">
+            <SearchBar name={name} />
+            <Loading />
+        </div>
+    );
 };
 
 export default Category;
