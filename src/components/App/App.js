03099cf31ffb6a9d291cad6ac21b6e2929b12c13
nembence <nemeth.bence@icloud.com>
2020-09-01 11:07:55 +0200

    Change App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import Main from "../Main/Main";
+import Home from "../Home/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
 
 function App() {
     return (
-        <>
+        <Router>
             <Header />
+            <Navbar />
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+            </Switch>
+            <Main />
             <Footer />
-        </>
+        </Router>
     );
 }
 
