e4c11f6981cc784a67cc2b06d3abe856c05a9eb4
nembence <nemeth.bence@icloud.com>
2020-09-02 12:39:04 +0200

    Change Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import url from "../../utilities/url";
+import useFetch from "../../utilities/useFetch";
 
 const Home = () => {
+    const [status, error, fetchedData] = useFetch(url.new_releases);
+
     return (
         <>
             <div className="content">Home</div>
