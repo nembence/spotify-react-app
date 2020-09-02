dbc93af5c592dcf9164c959e0902174051b42e15
nembence <nemeth.bence@icloud.com>
2020-09-02 14:42:19 +0200

    Change CardItem styling

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Card, Image } from "semantic-ui-react";
 
+const cardStyle = {
+    margin: "10px",
+    maxWidth: "300px",
+};
+
 const CardItem = ({ img, artist, title }) => {
     return (
-        <li>
-            <Card>
-                <Image src={img} wrapped ui={false} />
-                <Card.Content>
-                    <Card.Header>{artist}</Card.Header>
-                    <Card.Description>{title}</Card.Description>
-                </Card.Content>
-            </Card>
-        </li>
+        <Card style={cardStyle} className="column">
+            <Image src={img} wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>{artist}</Card.Header>
+                <Card.Description>{title}</Card.Description>
+            </Card.Content>
+        </Card>
     );
 };
 
