0e24bdd15b22cda43c2e877b36c9a07bbe08355c
nemethbence <DCdkm99dc>
2020-09-03 18:14:58 +0200

    Change PlaylistCard component

--- src/components/Cards/PlaylistCard/PlaylistCard.jsx
+++ src/components/Cards/PlaylistCard/PlaylistCard.jsx
@@ -1,33 +1,42 @@
 import React from "react";
 import { Card, Image } from "semantic-ui-react";
+import FavoriteStar from "../../FavoriteStar/FavoriteStar";
 
 const cardStyle = {
     margin: "10px",
-    maxWidth: "300px",
+    maxWidth: "300px"
 };
 
 const PlaylistCard = ({ data }) => {
     return (
-        <Card style={cardStyle} className="column">
+        <Card style={cardStyle} className='column'>
             <Image
                 src={data.images[0].url}
-                alt=""
+                alt=''
                 wrapped
-                as="a"
+                as='a'
                 ui={false}
                 href={data.external_urls.spotify}
-                target="_blank"
+                target='_blank'
             />
             <Card.Content>
                 <Card.Header>{data.name}</Card.Header>
                 <Card.Description
-                    as="a"
+                    as='a'
                     href={data.external_urls.spotify}
-                    target="_blank"
+                    target='_blank'
                 >
                     Tracks: {data.tracks.total}
                 </Card.Description>
             </Card.Content>
+            <FavoriteStar
+                category='playlist'
+                img={data.images[0].url}
+                imgUrl={data.external_urls.spotify}
+                header={"Tracks:" + data.tracks.total}
+                headerUrl={data.external_urls.spotify}
+                footer={data.name}
+            />
         </Card>
     );
 };
