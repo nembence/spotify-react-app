fff219b76e0979627034813fe79fbfe40eb417eb
nemethbence <DCdkm99dc>
2020-09-03 14:27:54 +0200

    Add Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 
 const Header = () => {
+    const headerStyle = {
+        backgroundColor: "#E18A07",
+        position: "absolute",
+        top: "0",
+        left: "0",
+        height: "70px",
+        width: "100%"
+    };
+
+    const headerName = {
+        fontSize: "55px",
+        fontWeight: "bolder",
+        position: "absolute",
+        left: "20px",
+        top: "20px",
+        color: "white"
+    };
+
     return (
-        <header>
-            <p>Spotify App</p>
-        </header>
+        <div style={headerStyle}>
+            <span style={headerName}>Spotify App</span>
+        </div>
     );
 };
 
