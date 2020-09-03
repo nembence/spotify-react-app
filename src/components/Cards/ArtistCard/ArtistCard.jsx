a56c0667d0287d1853909303ab8f04bcb20e83ab
magfabian <mag.fabian87@gmail.com>
2020-09-03 17:10:48 +0200

    Check if img is not defined

--- src/components/Cards/ArtistCard/ArtistCard.jsx
+++ src/components/Cards/ArtistCard/ArtistCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import url from "../../../utilities/url";
 import { Card, Image } from "semantic-ui-react";
 
 const cardStyle = {
@@ -9,15 +10,27 @@ const cardStyle = {
 const ArtistCard = ({ data }) => {
     return (
         <Card style={cardStyle} className="column">
-            <Image
-            // src={data.images[0].url}
-            // alt=""
-            // wrapped
-            // as="a"
-            // ui={false}
-            // href={data.external_urls.spotify}
-            // target="_blank"
-            />
+            {data.images[0] === undefined ? (
+                <Image
+                    src={url.noImg}
+                    alt=""
+                    wrapped
+                    as="a"
+                    ui={false}
+                    href={data.external_urls.spotify}
+                    target="_blank"
+                />
+            ) : (
+                <Image
+                    src={data.images[0].url}
+                    alt=""
+                    wrapped
+                    as="a"
+                    ui={false}
+                    href={data.external_urls.spotify}
+                    target="_blank"
+                />
+            )}
             <Card.Content>
                 <Card.Header>{data.name}</Card.Header>
                 <Card.Description
