a3865ee8cd964d088e735e955f5803ca44b49d04
nemethbence <DCdkm99dc>
2020-09-03 18:14:30 +0200

    Change Album card component

--- src/components/Cards/AbumCard/AlbumCard.jsx
+++ src/components/Cards/AbumCard/AlbumCard.jsx
@@ -1,33 +1,42 @@
 import React from "react";
 import { Card, Image } from "semantic-ui-react";
+import FavoriteStar from "../../FavoriteStar/FavoriteStar";
 
 const cardStyle = {
     margin: "10px",
-    maxWidth: "300px",
+    maxWidth: "300px"
 };
 
 const AlbumCard = ({ data }) => {
     return (
-        <Card style={cardStyle} className="column">
+        <Card style={cardStyle} className='column'>
             <Image
                 src={data.images[0].url}
-                alt=""
+                alt=''
                 wrapped
-                as="a"
+                as='a'
                 ui={false}
                 href={data.external_urls.spotify}
-                target="_blank"
+                target='_blank'
             />
             <Card.Content>
                 <Card.Header>{data.artists[0].name}</Card.Header>
                 <Card.Description
-                    as="a"
+                    as='a'
                     href={data.external_urls.spotify}
-                    target="_blank"
+                    target='_blank'
                 >
                     {data.name}
                 </Card.Description>
             </Card.Content>
+            <FavoriteStar
+                category='album'
+                img={data.images[0].url}
+                imgUrl={data.external_urls.spotify}
+                header={data.artists[0].name}
+                headerUrl={data.external_urls.spotify}
+                footer={data.name}
+            />
         </Card>
     );
 };
