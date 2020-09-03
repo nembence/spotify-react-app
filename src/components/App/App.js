c6559938c3a80656314a43335c76e3d8eceddba6
nemethbence <DCdkm99dc>
2020-09-03 18:33:35 +0200

    Refactor

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
 import Home from "../Home/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
--- src/components/SearchHandler/SearchHandler.jsx
+++ src/components/SearchHandler/SearchHandler.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import useFetch from "../../utilities/useFetch";
 import Loading from "../Loading/Loading";
 import Error from "../Error/Error";
